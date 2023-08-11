243d83d5d699d5b123a536982de105c0bcc2b3c0
Yash Garg <yashygkf@gmail.com>
2023-08-11 15:15:47 +0530

    fix: condition fixes

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -11,13 +11,13 @@ contract Exchange {
     address public immutable i_tokenAddress;
 
     constructor(address _tokenAddress) {
-        if(_tokenAddress != address(0)) revert Exchange__ZeroAddress();
+        if(_tokenAddress == address(0)) revert Exchange__ZeroAddress();
         i_tokenAddress = _tokenAddress;
     }
 
     function addLiquidity(uint256 _tokenAmount) external payable {
         IERC20 token = IERC20(i_tokenAddress);
-        if (token.transferFrom(msg.sender, address(this), _tokenAmount)) revert Exchange__AddingLiquidityFailed();
+        if (!token.transferFrom(msg.sender, address(this), _tokenAmount)) revert Exchange__AddingLiquidityFailed();
     }
 
     function getReserverBalance() external view returns (uint256) {
